67ad269d2eff7e796a4eddc771af12e2319505eb
Yug Gurnani <yug.gurnani091@gmail.com>
2021-05-22 20:56:44 +0530

    deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SortingVisualizer from './sortingVisualizer/sortingVisualizer';
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
